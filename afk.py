2db625aac481ced8121a1e1bc3374df1ec355ef0
Florian Ludwig <f.ludwig@greyrook.com>
2020-12-23 13:25:31 +0100

    basic version of afka

--- afk.py
+++ afk.py
@@ -4,30 +4,58 @@ import anre
 import time
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 
 class AFKArena:
     def __init__(self, ar: anre.Anre) -> None:
         self.ar = ar
+        self.activity_name = "com.lilithgame.hgame.gp"
+        self.current_screen = "campaign"
     
-    def close_app(self):
-        pass
-
     def start_app(self):
-        pass
-        
-    def wait_until_loaded(self):
-        while self.ar.get_pixel(450, 1775, update=True) != (0xcc, 0x92, 0x61):
-            time.sleep(1)
-        
+        self.ar.start_app(self.activity_name)
 
-    # t = time.time()
-    # img = ar.screencap_pillow()
-    # print(time.time() - t)
-    # pixel_data = img.load()
-    
-    # print(pixel_data)
+    def close_app(self):
+        self.ar.close_app(self.activity_name)
+
+    def wait_until_loaded(self):
+        while self.ar.get_pixel("50%", -1, update=True) != (242, 225, 145):
+            time.sleep(0.1)
+        
+        print("done loading")
+
+    def switch_tab(self, tab_name):
+        if self.current_screen == tab_name:
+            return
+
+        x = {
+            "ranhorn": "10%",
+            "dark_forest": "30%",
+            "campaign": "50%",
+            "heros": "70%",
+            "chat": "90%",
+        }[tab_name]
+
+        self.ar.tap(x, -10)
+
+        # TODO verify we successfully reached desired screen
+
+        self.current_screen = tab_name
+
+    def loot_afk_chest(self):
+        self.switch_tab("campaign")
+        self.ar.tap("50%", -450)
+        time.sleep(1)
+        self.ar.tap("60%", "65%")
+
+    def fight_campaign(self):
+        self.switch_tab("campaign")
+        self.ar.tap("50%", -280)
+
+    def close(self):
+        self.ar.close()
+
 
 @click.command()
 @click.option("--debug", is_flag=True, default=False)
@@ -48,7 +76,12 @@ def main(debug):
     afk.start_app()
     afk.wait_until_loaded()
 
-    time.sleep(160)
+    # afk.switch_tab("campaign")
+    # afk.switch_tab("ranhorn")
+    # afk.switch_tab("dark_forest")
+    afk.loot_afk_chest()
+
+    afk.close()
 
 
 
