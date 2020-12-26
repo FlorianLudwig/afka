54391f5e781bf8471f1bf9316ec5b45d4ff98875
Florian Ludwig <f.ludwig@greyrook.com>
2020-12-26 12:27:16 +0100

    logging

--- afka/afk.py
+++ afka/afk.py
@@ -13,6 +13,8 @@ class Screen(enum.Enum):
     pass
 
 
+logging.basicConfig(level=logging.INFO)
+
 LOG = logging.getLogger(__name__)
 AVOID_DOUBLE_TAB_DELAY = 0.1
 
@@ -30,10 +32,11 @@ class AFKArena:
         self.ar.close_app(self.activity_name)
 
     def wait_until_loaded(self):
+        LOG.info("waiting till app is loaded")
         while self.ar.get_pixel("50%", -1, update=True) != (242, 225, 145):
             time.sleep(0.1)
         
-        print("done loading")
+        LOG.info("afk arena done loading")
 
     def switch_to(self, target):
         LOG.debug("switch from %s to %s", self.current_screen, target)
@@ -76,6 +79,7 @@ class AFKArena:
         # TODO verify we successfully reached desired screen
         self.current_screen = target
 
+
     def tap_image(self, image_name, scale=1.0, threshold=0.9, timeout=60):
         collect = pkg_resources.resource_filename("afka", f"res/{image_name}.png")
         return self.ar.tap_image(collect, scale=scale, threshold=threshold, timeout=timeout)
@@ -89,12 +93,15 @@ class AFKArena:
         return self.ar.wait_for_image(collect, timeout=timeout, threshold=threshold, scale=scale)
 
     def loot_afk_chest(self):
+        LOG.info("starting loot_afk_chest")
         self.switch_to("campaign")
         self.ar.tap("50%", -450)  # tap on pile of loot
         self.tap_image("blue_button")  # tap collect button
         time.sleep(AVOID_DOUBLE_TAB_DELAY)
+        LOG.info("done loot_afk_chest")
 
     def loot_fast_rewards(self, spend_diamonds=False):
+        LOG.info("starting loot_fast_rewards")
         self.switch_to("campaign")
         self.tap_image("fast_rewards")
         self.wait_for_image("popup")
@@ -103,31 +110,35 @@ class AFKArena:
         if conf >= 0.8:
             if spend_diamonds:
                 self.ar.tap(x, y)
-                return
             else:
-                print("fast rewards only available for diamonds and spend_diamonds=False")
+                LOG.info("fast rewards only available for diamonds and spend_diamonds=False")
                 self.ar.tap(10, 10)
-                time.sleep(0.5)
+                time.sleep(AVOID_DOUBLE_TAB_DELAY)
+                LOG.info("done loot_fast_rewards")
                 return
 
-        self.tap_image("collect_yellow")
+        else:
+            self.tap_image("collect_yellow")
         time.sleep(AVOID_DOUBLE_TAB_DELAY)
         self.ar.tap(10, 10)
-        time.sleep(0.5)
+        time.sleep(AVOID_DOUBLE_TAB_DELAY)
+        LOG.info("done loot_fast_rewards")
 
     def guild_hunt(self):
+        LOG.info("starting guild_hunt")
         self.switch_to("guild_hunting")
         for _ in range(2):
             try:
                 self.tap_image("guild_hunt_challenge", timeout=10) # start challenge
             except ValueError:
                 print("Looks like guild hunting already done")
-                return
+                break
             time.sleep(0.1)
             self.tap_image("guild_hunt_challenge") # begin battle (confirms formation)
             time.sleep(60)
             self.tap_image("tap_to_close")
             self.tap_image("tap_to_close")
+        LOG.info("done guild_hunt")
 
     def click_all_image(self, image_name, scale=1.0, threshold=0.9, timeout=10):
         LOG.debug("Click all %s images", image_name)
@@ -139,12 +150,14 @@ class AFKArena:
                 return
 
     def collect_quest_rewards(self):
+        LOG.info("starting collect_quest_rewards")
         self.switch_to("quests_dailies")
         self.click_all_image("blue_button", threshold=0.7, scale=0.85)
 
         # self.switch_to("quests_weeklies")
 
         # self.switch_to("quests_campaign")
+        LOG.info("done collect_quest_rewards")
 
     def fight_campaign(self):
         self.switch_to("campaign")
